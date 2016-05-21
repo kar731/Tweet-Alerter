f30e744aa3043220aea60630bdf8310553510847
kar731 <seankana1@yahoo.com>
2016-05-21 11:23:08 -0700

    Fixed pep8 errors

--- TweetAlerter.py
+++ TweetAlerter.py
@@ -1,42 +1,45 @@
-import tweepy
+"""
+    This is used to alert the user of new tweets from a selected follower.
+"""
 import ctypes
+import credentials
+import tweepy
 
-CONSUMER_KEY = "key"
-CONSUMER_SECRET = "key"
-ACCESS_KEY = "key"
-ACCESS_SECRET = "key"
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+CONSUMER_KEY = credentials.CONSUMER_KEY
+CONSUMER_SECRET = credentials.CONSUMER_SECRET
+ACCESS_KEY = credentials.ACCESS_KEY
+ACCESS_SECRET = credentials.ACCESS_SECRET
+AUTH_KEY = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+AUTH_KEY.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+API = tweepy.API(AUTH_KEY)
 
-#if you want to hardcode it you can put the username as a string instead of input
-twitterAccount = input("What twitter account are you gonna watch? ")
-twitterAccountID = api.get_user(twitterAccount)
-twitterAccountID = twitterAccountID.id
+TWITTER_ACCOUNT = input("What twitter account are you gonna watch? ")
+TWITTER_ACCOUNT_ID = API.get_user(TWITTER_ACCOUNT)
+TWITTER_ACCOUNT_ID = TWITTER_ACCOUNT_ID.id
 
-def message(text, Title, Type):
-    MessageBox = ctypes.windll.user32.MessageBoxW
-    MessageBox(None, text, Title, Type)
+def message(text, title, boxtype=0):
+    """Used to popup a message to the user"""
+    message_box = ctypes.windll.user32.MessageBoxW
+    message_box(None, text, title, boxtype)
 
-class streamListener(tweepy.StreamListener):
+class StreamListener(tweepy.StreamListener):
+    """Used to get new tweets as they are sent out."""
     def on_status(self, status):
         user = status.user.screen_name
         print(user + ":    " + status.text)
-        print("http://www.twitter.com/{0}".format(user) + "\n")
-        message('{0} tweeted: {1}.'.format(user, status.text), 'NEW TWEET', 0)
+        print("http://www.twitter.com/{0} \n".format(user))
+        message('{0} tweeted: {1}.'.format(user, status.text), 'NEW TWEET')
 
     def on_error(self, status_code):
         print('Got an error with status code: ' + str(status_code))
         return True
- 
+
     def on_timeout(self):
         print('Timeout...')
         return True
 
-
 if __name__ == '__main__':
-    listener = streamListener()
- 
-    stream = tweepy.Stream(auth, listener)
-    stream.filter(follow=[str(twitterAccountID)])
-
+    LISTENER = StreamListener()
+    STREAM = tweepy.Stream(AUTH_KEY, LISTENER)
+    STREAM.filter(follow=[str(TWITTER_ACCOUNT_ID)])
+    
